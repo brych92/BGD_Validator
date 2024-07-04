f1a2f222e30729094a0d0dbb8a47e1189a5f23c2
brych92 <brych92@gmail.com>
2024-07-04 19:26:02 +0300

    додав батьківське вікно

--- init.py
+++ init.py
@@ -4,11 +4,21 @@ sys.path.append(r'C:\Users\brych\OneDrive\Документы\01 Робота\98 
 
 #del ResultWindow
 
-import ResultWindow, resultStructure
-imp.reload(ResultWindow)
-imp.reload(resultStructure)
-from ResultWindow import *
-from resultStructure import result
+#from qgis.utils import iface
 
-window = ResultWindow(result)
+import ResultWindow, resultStructure
+from initialize_script import run_validator, get_layer_list_for_validator
+#from initialize_script import run_validator, get_layer_list_for_validator
+imp.reload(ResultWindow) #юзається шоб скинути кеш скрипта коли ти шось там міняєш
+imp.reload(resultStructure)#юзається шоб скинути кеш скрипта коли ти шось там міняєш
+#imp.reload(initialize_script)#юзається шоб скинути кеш скрипта коли ти шось там міняєш
+from ResultWindow import *
+#from resultStructure import result
+
+selected_layers = iface.layerTreeView().selectedLayersRecursive()
+
+result = run_validator(get_layer_list_for_validator(selected_layers))
+
+
+window = ResultWindow(result, parent=iface.mainWindow())
 window.show()
