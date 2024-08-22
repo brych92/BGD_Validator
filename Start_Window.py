36a5e9adf1233a3548b987b4e40231c5a78ba58f
brych92 <brych92@gmail.com>
2024-08-23 00:22:09 +0300

    пекельні борошна -Підшаманив форматування -Видалив лишні імпорти -Виправив проблему з неправильним шаром шо давався на перевірку -Переробив передачу СК в валідатор -Додав підтримку автоконвертації csv в json -

--- Start_Window.py
+++ Start_Window.py
@@ -1,9 +1,9 @@
 
 from importlib import reload
-from tkinter import S
+
 from typing import Union, cast 
 from PyQt5.QtWidgets import QTreeWidget, QTreeWidgetItem, QApplication, QVBoxLayout, QHBoxLayout, \
-    QWidget, QDialog, QTreeView, QPushButton, QFileDialog, QMenu, QFrame, QComboBox
+    QWidget, QDialog, QTreeView, QPushButton, QFileDialog, QMenu, QFrame, QComboBox, QMessageBox
 from PyQt5.QtCore import Qt, QMimeData, QSize
 from numpy import unicode_
 from qgis.core import QgsProject, QgsLayerTreeLayer, QgsLayerTreeModel, QgsLayerTree, QgsProviderRegistry, QgsVectorLayer
@@ -14,16 +14,29 @@ from osgeo import ogr
 import json
 sys.path.append(r'C:\Users\brych\OneDrive\Документы\01 Робота\98 Сторонні проекти\ua mbd team\Плагіни\Перевірка на МБД\BGD_Validator')
 
-import initialize_script
+# import initialize_script
+import csv_to_json_structure_converter
+reload(csv_to_json_structure_converter)
+from csv_to_json_structure_converter import Csv_to_json_structure_converter
+
+# reload(initialize_script)
 import Result_Window
-
-reload(initialize_script)
 reload(Result_Window)
-
-from initialize_script import run_validator
 from Result_Window import ResultWindow
+
+import checker_class
+reload(checker_class)
+from checker_class import EDRA_exchange_layer_checker, EDRA_validator
+
+logging = True
+# from initialize_script import run_validator
 # import initialize_script
 # import Result_Window
+
+def log(text: str) -> None:
+    if logging:
+        print(text)
+
 
 def get_real_layer_name(layer: QgsVectorLayer) -> str:
     """
@@ -54,6 +67,78 @@ def get_real_layer_name(layer: QgsVectorLayer) -> str:
         
     return source_layer_name
 
+def run_validator(layers:dict, structure_path:str, domains_path:str):
+    def get_guid_dict(layers):
+        return "Ich bin schoen!"
+    """
+    Запустити валідатор для шарів.
+
+    Args:
+        layers (dict): Словник шарів. Кожен шар - це словник з ключами:
+            - layer_crs (str): Система координат шару.
+            - layer_name (str): Ім'я шару.
+            - path (str): Шлях до шару.
+            - driver_name (str): Ім'я драйвера, який використовувався для відкриття шару.
+        structure_path (str): Шлях до файлу структури.
+        domains_path (str): Шлях до файлу доменів.
+
+    Returns:
+        dict: Словник з результатами валідатору.
+            - layers (dict): Словник результатів валідатору для кожного шару.
+            - exchange_format_error (list): Список шарів з помилками формату обміну.
+            - missing_layers (list): Список відсутніх шарів.
+    """
+
+
+    all_layers_check_result_dict = {}
+    all_layers_check_result_dict['layers'] = {}
+    all_layers_check_result_dict['exchange_format_error'] = []
+    all_layers_check_result_dict['missing_layers'] = []
+    
+    
+
+    for id in layers:
+        dataSource = ogr.Open(layers[id]['path'], 0) # 0 means read-only. 1 means writeable.
+        
+        if dataSource.GetDriver().GetName() in ['OpenFileGDB', 'GPKG']:
+            layer = dataSource.GetLayerByName(layers[id]['layer_name'])
+        else:
+            layer = dataSource.GetLayer()
+        
+        group = 'EDRA'
+        layer_real_name = layers[id]['layer_real_name']
+        
+        converter = Csv_to_json_structure_converter(os.path.dirname(structure_path))
+
+        structure = converter.create_structure_json() 
+        domains = converter.create_domain_json()
+        # with open(structure_path, 'r', encoding='utf-8') as f: 
+        #     structure = json.loads(f.read())
+        
+        # with open(domains_path, 'r', encoding='utf-8') as f: 
+        #     domains = json.loads(f.read())
+            
+        if layer is None:
+            raise AttributeError(f'Не вдалося відкрити шар {layers[id]["path"]}')
+        
+        layer_EDRA_valid_class = EDRA_validator(
+            layer = layer,
+            layer_exchange_group=group,
+            layer_exchange_name=layer_real_name,
+            structure_json=structure,
+            domains_json=domains)
+        
+        validate_checker = EDRA_exchange_layer_checker(
+            layer_EDRA_valid_class = layer_EDRA_valid_class,
+            layer_props = layers[id],
+            layer_id = id)
+        
+        validate_result = validate_checker.run()
+        
+        all_layers_check_result_dict['layers'][list(validate_result.keys())[0]] = validate_result[list(validate_result.keys())[0]]
+        
+    return all_layers_check_result_dict
+
 class customlayerListWidget(QTreeWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
@@ -78,9 +163,18 @@ class customlayerListWidget(QTreeWidget):
         
         super().addTopLevelItem(newItem)
 
-
 class layerItem(QTreeWidgetItem):
+    """
+    Елемент списку шарів, що додається до customlayerListWidget.
+    """
     def __init__(self, id:str, visible_name:str, path:str, real_name:str):
+        """
+        Аргументи:
+            id (str): ідентифікатор шару (унікальний для кожного шару)
+            visible_name (str): назва шару, що відображається у списку
+            path (str): шлях до файлу шару
+            real_name (str): реальна назва шару, що використовується при перевірці
+        """
         super().__init__()
         
         if id == '':
@@ -217,7 +311,7 @@ class MainWindow(QDialog):
             {
                 'structure': 'C:/Users/brych/OneDrive/Документы/01 Робота/98 Сторонні проекти/ua mbd team/Плагіни/Перевірка на МБД/BGD_Validator/EDRA_structure/structure_bgd3.json',
                 'domains': 'C:/Users/brych/OneDrive/Документы/01 Робота/98 Сторонні проекти/ua mbd team/Плагіни/Перевірка на МБД/BGD_Validator/EDRA_structure/domain.json',
-                'crs': ['EPSG:4326','EPSG:5560', 'EPSG:5561','EPSG:9821', 'EPSG:9831', 'EPSG:9832', 'EPSG:9833', 'EPSG:9834',
+                'crs': ['EPSG:4326;EPSG:5560','EPSG:5560', 'EPSG:5561','EPSG:9821', 'EPSG:9831', 'EPSG:9832', 'EPSG:9833', 'EPSG:9834',
                         'EPSG:9835', 'EPSG:9836', 'EPSG:9837', 'EPSG:9838', 'EPSG:9839',
                         'EPSG:9840', 'EPSG:9841', 'EPSG:9842', 'EPSG:9843', 'EPSG:9851',
                         'EPSG:9852', 'EPSG:9853', 'EPSG:9854', 'EPSG:9855', 'EPSG:9856',
@@ -260,27 +354,36 @@ class MainWindow(QDialog):
         for i in range(self.layer_list_widget.topLevelItemCount()):
             layer = self.layer_list_widget.topLevelItem(i)
             layer = cast(layerItem, layer)
-            layers_dict[layer.getID()] = {'layer_name': layer.getRealName(), 'path': layer.getPath(), 'layer_real_name': layer.getRealName(), 'layer_crs': self.crs_combo_box.currentText()}
+            layers_dict[layer.getID()] = {
+                'layer_name': layer.getRealName(),
+                'path': layer.getPath(),
+                'layer_real_name': layer.getRealName(),
+                'required_crs': self.crs_combo_box.currentText().split(';')
+                }
 
             #print(f"{layer.layerID} {layer.layerName} {layer.layerPath} {type(layer.layerPath)}")
         
         print('Вхідний список шарів:')
         print(json.dumps(layers_dict, indent=4,ensure_ascii=False))        
-        result_structure = run_validator(layer = layers_dict,structure_path=self.strutures[self.BGD_type_combo_box.currentText()]['structure'],domains_path=self.strutures[self.BGD_type_combo_box.currentText()]['domains'])
+        result_structure = run_validator(
+            layers = layers_dict,
+            structure_path=self.strutures[self.BGD_type_combo_box.currentText()]['structure'],
+            domains_path=self.strutures[self.BGD_type_combo_box.currentText()]['domains'])
 
         print('\n\n\n\n\nВивід')
-        print(json.dumps(result_structure, indent=4, ensure_ascii=False)) 
+        print(json.dumps(result_structure, indent=4, ensure_ascii=False))
         window = ResultWindow(result_structure, parent=iface.mainWindow())
         window.show()
-        #structure_bgd_file_path = 'C:/Users/brych/OneDrive/Документы/01 Робота/98 Сторонні проекти/ua mbd team/Плагіни/Перевірка на МБД/BGD_Validator/EDRA_structure/structure_bgd3.json'
-        #domains_bgd_file_path = r'C:\Users\brych\OneDrive\Документы\01 Робота\98 Сторонні проекти\ua mbd team\Плагіни\Перевірка на МБД\BGD_Validator\EDRA_structure\structure_bgd3.json'
-        
 
     def printSelectedLayerData(self):
         for item in self.layer_list_widget.selectedItems():
             print(item.get_layer_vlaue())
 
     def openFiles(self):
+        def random_id(layerName):
+            randomid = ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(36))
+            return f"⁂{layerName}_{randomid}"
+        
         pathArr=QFileDialog.getOpenFileNames(None,"Виберіть файл(файли) для імпорту", self.folder_path, "Geopackage (*.gpkg);;GeoJSON (*.json, *.geojson);;GeoDatabase (*.gdb)")[0]
         if pathArr==[]:
             print('Нічого не вибрано!')
@@ -292,49 +395,47 @@ class MainWindow(QDialog):
         layersList = []
 
         for path in pathArr:
-            if type=='json' or type=='geojson':
+            if type in ['json','geojson']:
                 #print(path)
-                datasource  = ogr.Open(path, 0)
-                if datasource is None:
+                ds  = ogr.Open(path, 0)
+                if ds is None:
                     print('Could not open %s' % (path))
                     continue
-                layer = datasource.GetLayer()
+                layer = ds.GetLayer()
                 layerName = layer.GetName()
-                randomid = ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(36))
-                id = f"⁂{layerName}_{randomid}"
+                id = random_id(layerName)
                 layersList.append(layerItem(id=id, visible_name=layerName, real_name=layerName, path=path))
 
             elif type=='gpkg':
                 ds = ogr.Open(path)
+                if ds is None:
+                    QMessageBox.critical(None, "Помилка", f"Файл {path} пошкоджено")
+                
                 tempLayersList = []
                 for i in range(ds.GetLayerCount()):
                     layer = ds.GetLayerByIndex(i)
                     layerName = layer.GetName()
-                    randomid = ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(36))
-                    id = f"⁂{layerName}_{randomid}"
+                    id = random_id(layerName)
+                    layerpath = path
+
                     tempLayersList.append(layerItem(id=id, visible_name=layerName, real_name=layerName, path=path))
                 
                 lsDialog = layerSelectionDialog(tempLayersList, parent=self)
                 
                 if lsDialog.exec_() == 1:
                     selectedItems = lsDialog.get_selected_layers()
+                    errors_list = []
                     for item in selectedItems:
+                        datachecker =  ogr.Open(path, 0)
+                        layer = datachecker.GetLayerByName(item.getRealName())
+                        if layer is None:
+                            errors_list.append(item)
+                            continue
                         layersList.append(item)
-                lsDialog = None
                 
-            #     layer  = ogr.Open(path, 0).getLayer()
-            #     layerName = layer.GetName()
-            #     randomid = ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(36))
-            #     id = f"⁂{layerName}_{randomid}"
-            #     LayersList.append(layerItem(id, layerName, path))
+                if len(errors_list) > 0: QMessageBox.critical(None, "Помилка", '\r\n'.join([f"Шар '{item.getLayerName()}', файлу '{path}' пошкоджено" for item in errors_list]))
+                lsDialog = None
 
-            # elif type=='gdb':
-            #     layer  = ogr.Open(path, 0).getLayer()
-            #     layerName = layer.GetName()
-            #     randomid = ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(36))
-            #     id = f"⁂{layerName}_{randomid}"
-            #     LayersList.append(layerItem(id, layerName, path))
-        
         self.layer_list_widget.clear()
         for layer in layersList:
             self.layer_list_widget.addTopLevelItem(layer)
