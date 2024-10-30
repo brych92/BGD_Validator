88e97abf3e7326fe77399b990ab486c4b1847e2c
Bohdan <bogdan.zhuravel.00@gmail.com>
2024-10-30 21:30:48 +0200

    result_v2 зробив урізаним
    result_v2 зробив урізаним, щоб додавати результат первірки

--- ua_orthodoxy_validator/resultStructure_v2.py
+++ ua_orthodoxy_validator/resultStructure_v2.py
@@ -1,18 +1,17 @@
-result_v2 = [
-    {
+result_v2 = [{
         'type' : 'container',
         'item_name' : "Загальні помилки",
         'subitems' : [
             {
                 'type' : 'inspection',
-                'inspection_type_name' : "Перевірка наявності обов’язкових шарів",
+                'inspetcion_type_name' : "Перевірка наявності обов’язкових шарів",
                 'item_name' : "Відсутній шар 'Імя шару', що вимагається структурою",
                 'criticity' : 1,
                 'help_url' : "https://e-construction.gov.ua/laws_detail/3260441209981634046?doc_type=2"
             },
             {
                 'type' : 'inspection',
-                'inspection_type_name' : "Перевірка наявності обов’язкових шарів",
+                'inspetcion_type_name' : "Перевірка наявності обов’язкових шарів",
                 'item_name' : "Відсутній шар 'Імя шару 2', що вимагається структурою",
                 'criticity' : 1,
                 'help_url' : "https://e-construction.gov.ua/laws_detail/3260441209981634046?doc_type=2"
@@ -20,14 +19,76 @@ result_v2 = [
             { #а можна отак, тоді вони просто як підпункти виведуться, але це тіки при умові, шо нас влаштовує, що меню правої кнопки буде вести на один і той самий URL
                 'type' : 'inspection',
                 'inspection_category' : 'general',
-                'inspection_type_name' : "Перевірка наявності обов’язкових шарів",
+                'inspetcion_type_name' : "Перевірка наявності обов’язкових шарів",
                 'item_name' : [
                     "Відсутній шар 'Імя шару 1', що вимагається структурою",
                     "Відсутній шар 'Імя шару 2', що вимагається структурою",
                     "Відсутній шар 'Імя шару 3', що вимагається структурою",
                     "Відсутній шар 'Імя шару 4', що вимагається структурою"
                 ],
-                'criticity' : 2,
+                'criticity' : 1,
+                'help_url' : "https://e-construction.gov.ua/laws_detail/3260441209981634046?doc_type=2"
+            }
+        ]
+    },
+    {
+        'type' : 'file',
+        'item_name' : "Файл 'Імя файлу'",
+        'related_file_path' : r"C:\Users\brych\OneDrive\Документы\01 Робота\98 Сторонні проекти\ua mbd team\Плагіни\Перевірка на МБД\Для тестування\EDRA від Богдана\streets.geojson",
+        'subitems' : [                
+            {
+                'type' : 'inspection',
+                'inspetcion_type_name' : "Перевірка формату геоданих",
+                'item_name' : "Формат файлу 'GDB' не відповідає вимогам, вимагається 'GEOJSON' або 'SHP'",
+                'criticity' : 1,
+                'help_url' : "https://genius.com/Evanescence-lithium-lyrics"
+            },
+            {
+                'type' : 'inspection',
+                'inspetcion_type_name' : "Перевірка кодування файлу",
+                'item_name' : "Кодування файлу 'WINDOWS-1251' не відповідає вимогам, вимагається 'UTF-8'",
+                'criticity' : 1,
+                'help_url' : "https://genius.com/Nirvana-rape-me-lyrics"
+            },
+            {
+                'type' : 'inspection',
+                'inspetcion_type_name' : "Перевірка на наявність даних",
+                'item_name' : "У файлі 'Імя файлу' наявні дані",
+                'criticity' : 0
+            },       ] }
+]
+
+
+result_v23 = [
+    {
+        'type' : 'container',
+        'item_name' : "Загальні помилки",
+        'subitems' : [
+            {
+                'type' : 'inspection',
+                'inspetcion_type_name' : "Перевірка наявності обов’язкових шарів",
+                'item_name' : "Відсутній шар 'Імя шару', що вимагається структурою",
+                'criticity' : 1,
+                'help_url' : "https://e-construction.gov.ua/laws_detail/3260441209981634046?doc_type=2"
+            },
+            {
+                'type' : 'inspection',
+                'inspetcion_type_name' : "Перевірка наявності обов’язкових шарів",
+                'item_name' : "Відсутній шар 'Імя шару 2', що вимагається структурою",
+                'criticity' : 1,
+                'help_url' : "https://e-construction.gov.ua/laws_detail/3260441209981634046?doc_type=2"
+            },
+            { #а можна отак, тоді вони просто як підпункти виведуться, але це тіки при умові, шо нас влаштовує, що меню правої кнопки буде вести на один і той самий URL
+                'type' : 'inspection',
+                'inspection_category' : 'general',
+                'inspetcion_type_name' : "Перевірка наявності обов’язкових шарів",
+                'item_name' : [
+                    "Відсутній шар 'Імя шару 1', що вимагається структурою",
+                    "Відсутній шар 'Імя шару 2', що вимагається структурою",
+                    "Відсутній шар 'Імя шару 3', що вимагається структурою",
+                    "Відсутній шар 'Імя шару 4', що вимагається структурою"
+                ],
+                'criticity' : 1,
                 'help_url' : "https://e-construction.gov.ua/laws_detail/3260441209981634046?doc_type=2"
             }
         ]
@@ -39,13 +100,13 @@ result_v2 = [
         'subitems' : [
             {
                 'type' : 'inspection',
-                'inspection_type_name' : "Перевірка формату геоданих",
+                'inspetcion_type_name' : "Перевірка формату геоданих",
                 'item_name' : "Формат файлу 'GeoJson' відповідає структурі",
                 'criticity' : 0
             },
             {
                 'type' : 'inspection',
-                'inspection_type_name' : "Перевірка на наявність даних",
+                'inspetcion_type_name' : "Перевірка на наявність даних",
                 'item_name' : "У файлі 'Імя файлу' відсутні дані",
                 'criticity' : 1,
                 'help_url' : "https://genius.com/Nirvana-smells-like-teen-spirit-lyrics"
@@ -60,21 +121,21 @@ result_v2 = [
         'subitems' : [                
             {
                 'type' : 'inspection',
-                'inspection_type_name' : "Перевірка формату геоданих",
+                'inspetcion_type_name' : "Перевірка формату геоданих",
                 'item_name' : "Формат файлу 'GDB' не відповідає вимогам, вимагається 'GEOJSON' або 'SHP'",
-                'criticity' : 2,
+                'criticity' : 1,
                 'help_url' : "https://genius.com/Evanescence-lithium-lyrics"
             },
             {
                 'type' : 'inspection',
-                'inspection_type_name' : "Перевірка кодування файлу",
+                'inspetcion_type_name' : "Перевірка кодування файлу",
                 'item_name' : "Кодування файлу 'WINDOWS-1251' не відповідає вимогам, вимагається 'UTF-8'",
                 'criticity' : 1,
                 'help_url' : "https://genius.com/Nirvana-rape-me-lyrics"
             },
             {
                 'type' : 'inspection',
-                'inspection_type_name' : "Перевірка на наявність даних",
+                'inspetcion_type_name' : "Перевірка на наявність даних",
                 'item_name' : "У файлі 'Імя файлу' наявні дані",
                 'criticity' : 0
             },
@@ -87,31 +148,31 @@ result_v2 = [
                 'subitems' : [
                     {
                         'type' : 'inspection',
-                        'inspection_type_name' : "Перевірка на тип геометрії",
+                        'inspetcion_type_name' : "Перевірка на тип геометрії",
                         'item_name' : "Тип геометрії шару 'Point' не відповідає структурі, вимагається 'LineString'",
-                        'criticity' : 2,
+                        'criticity' : 1,
                         'help_url' : "https://genius.com/Rammstein-du-hast-lyrics"
                     },
                     {
                         'type' : 'inspection',
-                        'inspection_type_name' : "Перевірка на систему координат шару",
+                        'inspetcion_type_name' : "Перевірка на систему координат шару",
                         'item_tooltip' : "А шо тии думав, шо тут буде підказка?",
                         'item_name' : "Система координат шару 'EPSG:9999' не відповідає структурі, вимагається 'EPSG:4326'",
-                        'criticity' : 2,
+                        'criticity' : 1,
                         'help_url' : "https://genius.com/Nirvana-rape-me-lyrics"
                     },
                     {
                         'type' : 'inspection',
-                        'inspection_type_name' : "Перевірка на відсутність полів",
+                        'inspetcion_type_name' : "Перевірка на відсутність полів",
                         'item_name' : "В шарі відсутнє поле атрибуту 'Імя поля', що вимагається по структурі",
                         'criticity' : 1,
                         'help_url' : "https://genius.com/Evanescence-lithium-lyrics"
                     },
                     {
                         'type' : 'inspection',
-                        'inspection_type_name' : "Перевірка на відсутність полів",
+                        'inspetcion_type_name' : "Перевірка на відсутність полів",
                         'item_name' : "В шарі відсутнє поле атрибуту 'Імя 2 поля', що вимагається по структурі",
-                        'criticity' : 2,
+                        'criticity' : 1,
                         'help_url' : "https://genius.com/Evanescence-lithium-lyrics"
                     },
                     {
@@ -125,21 +186,21 @@ result_v2 = [
                                 'subitems' : [
                                     {
                                         'type' : 'inspection',
-                                        'inspection_type_name' : "Перевірка наявності атрибутів",
+                                        'inspetcion_type_name' : "Перевірка наявності атрибутів",
                                         'item_name' : "В об'єкті не заповнений атрибут 'number', що вимагається по структурі",
                                         'criticity' : 1,
                                         'help_url' : "https://genius.com/Evanescence-lithium-lyrics"
                                     },
                                     {
                                         'type' : 'inspection',
-                                        'inspection_type_name' : "Перевірка наявності атрибутів",
+                                        'inspetcion_type_name' : "Перевірка наявності атрибутів",
                                         'item_name' : "В об'єкті не заповнений атрибут 'street', що вимагається по структурі",
                                         'criticity' : 1,
                                         'help_url' : "https://genius.com/Evanescence-lithium-lyrics"
                                     },
                                     {
                                         'type' : 'inspection',
-                                        'inspection_type_name' : "Перевірка наявності атрибутів",
+                                        'inspetcion_type_name' : "Перевірка наявності атрибутів",
                                         'item_name' : "В об'єкті не заповнений атрибут 'street', що вимагається по структурі",
                                         'criticity' : 1,
                                         'help_url' : "https://genius.com/Evanescence-lithium-lyrics"
@@ -148,21 +209,21 @@ result_v2 = [
                             },                            
                             {
                                 'type' : 'inspection',
-                                'inspection_type_name' : "Перевірка типу геометрії об’єкта",
+                                'inspetcion_type_name' : "Перевірка типу геометрії об’єкта",
                                 'item_name' :  "Тип геометрії об'єкту 'Point' не відповідає структурі, вимагається 'LineString'",
-                                'criticity' : 2,
+                                'criticity' : 1,
                                 'help_url' : "https://genius.com/Evanescence-lithium-lyrics"
                             },
                             {
                                 'type' : 'inspection',
-                                'inspection_type_name' : "Перевірка на наявність об’єктів в шарі",
+                                'inspetcion_type_name' : "Перевірка на наявність об’єктів в шарі",
                                 'item_name' :  "В шарі відсутні об’єкти, що вимагається по структурі",
                                 'criticity' : 1,
                                 'help_url' : "https://genius.com/Evanescence-lithium-lyrics"
                             },
                             {
                                 'type' : 'inspection',
-                                'inspection_type_name' : "Перевірка дублювання GUID (унікального атрибуту)",
+                                'inspetcion_type_name' : "Перевірка дублювання GUID (унікального атрибуту)",
                                 'item_name' :  "GUID не унікальний в межах шарів що валідуються",
                                 'corresponding_objects' : [
                                     {
@@ -176,14 +237,14 @@ result_v2 = [
                             },
                             {
                                 'type' : 'inspection',
-                                'inspection_type_name' : "Перевірка чи значення атрибуту є в переліку доменів",
+                                'inspetcion_type_name' : "Перевірка чи значення атрибуту є в переліку доменів",
                                 'item_name' :  "Значення атрибуту 'Імя' не відповідає класифікатору, що вимагається по структурі",
                                 'criticity' : 1,
                                 'help_url' : "https://genius.com/Evanescence-lithium-lyrics"
                             },
                             {
                                 'type' : 'inspection',
-                                'inspection_type_name' : "Перевірка чи значення атрибуту є в переліку доменів",
+                                'inspetcion_type_name' : "Перевірка чи значення атрибуту є в переліку доменів",
                                 'item_name' :  "Значення атрибуту 'Імя' не відповідає класифікатору, що вимагається по структурі",
                                 'criticity' : 1,
                                 'help_url' : "https://genius.com/Evanescence-lithium-lyrics"
